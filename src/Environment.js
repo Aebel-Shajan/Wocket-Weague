f90d237db1b42bea27d10b5f8884a1f223494f00
Aebel Shajan <aebelshajan@gmail.com>
2024-02-25 10:54:39 +0000

    Add shader to floor. Make floor a property of the environment.

--- src/Environment.js
+++ src/Environment.js
@@ -4,15 +4,94 @@ import { GameObject } from "./GameObject";
 
 export class Environment {
     floor;
+    floorMaterial;
     scene;
     world;
     constructor(scene, world) {
         this.scene = scene;
         this.world = world;
-        this.floor = createFloorObject();
+        this.floor = this.createFloorObject();
         this.floor.addObjectTo(scene, world);
         setupBackground(scene);
         setupLighting(scene);
+    }
+    createFloorObject() {
+
+     
+        const vertexShader = `
+        varying vec2 vUv;
+        uniform float time;
+        
+          void main() {
+      
+          vUv = uv;
+          
+          // VERTEX POSITION
+          
+          vec4 mvPosition = vec4( position, 1.0 );
+          #ifdef USE_INSTANCING
+              mvPosition = instanceMatrix * mvPosition;
+          #endif
+          
+          // DISPLACEMENT
+          
+          // here the displacement is made stronger on the blades tips.
+          float dispPower = 1.0 - cos( uv.y * 3.1416 / 2.0 );
+          
+          float displacement = sin( mvPosition.z + time * 10.0 ) * ( 0.1 * dispPower );
+          mvPosition.z += displacement;
+          
+          //
+          
+          vec4 modelViewPosition = modelViewMatrix * mvPosition;
+          gl_Position = projectionMatrix * modelViewPosition;
+      
+          }
+      `;
+      
+      const fragmentShader = `
+        varying vec2 vUv;
+        
+        void main() {
+            vec3 baseColor = vec3( 0.41, 1.0, 0.5 );
+          float clarity = ( vUv.y * 0.5 ) + 0.5;
+          gl_FragColor = vec4( baseColor * clarity, 1 );
+        }
+      `;
+      
+      const uniforms = {
+          time: {
+            value: 0
+        }
+      }
+      
+      const leavesMaterial = new THREE.ShaderMaterial({
+          vertexShader,
+        fragmentShader,
+        uniforms,
+        side: THREE.DoubleSide
+      });
+      
+    
+        // Create a material using the custom shader
+        this.floorMaterial = leavesMaterial;
+    
+        // Create a ground plane
+        const floorMesh = new THREE.Mesh(
+            new THREE.PlaneGeometry(1000, 1000),
+            this.floorMaterial
+        );
+        const floorPhysics = {
+            shape: new CANNON.Plane(),
+            type: CANNON.Body.STATIC,
+            material: new CANNON.Material({
+                friction: 0
+            }),
+        }
+        const floorObject = new GameObject(floorMesh, floorPhysics);
+        floorObject.rotateAroundAxis(new THREE.Vector3(1, 0, 0), - Math.PI / 2);
+        floorObject.updateMesh();
+        return floorObject;
     }
 }
 
@@ -39,23 +118,4 @@ function setupBackground(scene) {
         'pz.jpg', 'nz.jpg'
     ]);
     scene.background = texturefloor;
-}
-
-function createFloorObject() {
-    // Create a ground plane
-    const floorMesh = new THREE.Mesh(
-        new THREE.PlaneGeometry(1000, 1000),
-        new THREE.MeshStandardMaterial({ roughness: 0.9, color: 0xaaaaaa })
-    );
-    const floorPhysics = {
-        shape: new CANNON.Plane(),
-        type: CANNON.Body.STATIC,
-        material: new CANNON.Material({
-            friction: 0
-        }),
-    }
-    const floorObject = new GameObject(floorMesh, floorPhysics);
-    floorObject.rotateAroundAxis(new THREE.Vector3(1, 0, 0), - Math.PI / 2);
-    floorObject.updateMesh();
-    return floorObject;
 }
