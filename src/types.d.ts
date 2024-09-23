05cfa125ab430de26a447fb19e055c224c5ed77e
Juan A <30204147+jaas666@users.noreply.github.com>
2024-09-23 18:08:31 -0500

    Biome fixed files

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,48 +1,48 @@
-import RAPIER from "@dimforge/rapier3d-compat";
+import type RAPIER from "@dimforge/rapier3d-compat";
 
 // This makes me cringe looking back
 
 interface RendererOptions {
-    width?: number;
-    height?: number;
-    enableVR?: boolean;
-    pixelRatio?: number;
-    cameraOptions?: CameraOptions;
-    setupFullScreenCanvas?: boolean;
-    canvas?: HTMLCanvasElement;
-    beforeRender?: (args: { deltaTimeInSec: number, time: number}) => void;
+	width?: number;
+	height?: number;
+	enableVR?: boolean;
+	pixelRatio?: number;
+	cameraOptions?: CameraOptions;
+	setupFullScreenCanvas?: boolean;
+	canvas?: HTMLCanvasElement;
+	beforeRender?: (args: { deltaTimeInSec: number; time: number }) => void;
 }
 
 interface CameraOptions {
-    fov?: number;
-    aspect?: number;
-    near?: number;
-    far?: number;
+	fov?: number;
+	aspect?: number;
+	near?: number;
+	far?: number;
 }
 
 interface RigidBodyData {
-    colliderDesc: RAPIER.ColliderDesc;
-    rigidBodyDesc: RAPIER.RigidBodyDesc;
+	colliderDesc: RAPIER.ColliderDesc;
+	rigidBodyDesc: RAPIER.RigidBodyDesc;
 }
 
 interface Vec3 {
-    x: number, 
-    y: number, 
-    z: number
+	x: number;
+	y: number;
+	z: number;
 }
 
 interface movementInput {
-    sideward?: number,
-    upward?: number,
-    forward?: number,
-    pitch?: number,
-    yaw?: number,
-    roll?: number
+	sideward?: number;
+	upward?: number;
+	forward?: number;
+	pitch?: number;
+	yaw?: number;
+	roll?: number;
 }
 
 interface carInput extends movementInput {
-    forward: number,
-    yaw: number,
-    roll: number,
-    upward: number
+	forward: number;
+	yaw: number;
+	roll: number;
+	upward: number;
 }
