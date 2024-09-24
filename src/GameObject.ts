b80ee7b0fb5158915db6342a98a51b8b8b5d642b
Aebel Shajan <67755450+Aebel-Shajan@users.noreply.github.com>
2024-09-24 16:50:55 +0100

    Add tsdocs to GameObject.ts.

--- src/GameObject.ts
+++ src/GameObject.ts
@@ -1,16 +1,27 @@
 import type RAPIER from "@dimforge/rapier3d-compat";
-// https://github.com/WesUnwin/three-game-engine/blob/main/src/GameObject.ts
 import type * as THREE from "three";
 
 import type Scene from "./Scene";
 import type { RigidBodyData } from "./types";
 
+/**
+ * Represents a game object in the scene.
+ *
+ * @see [three-game-engine](https://github.com/WesUnwin/three-game-engine/blob/main/src/GameObject.ts)
+ * copied hella code from there.
+ */
 class GameObject {
 	scene: Scene | null;
 	threeJSGroup: THREE.Mesh;
 	rigidBodyData: RigidBodyData;
 	rapierRigidBody: RAPIER.RigidBody | null;
 
+	/**
+	 * Creates a new GameObject instance.
+	 * @param scene - The scene the game object belongs to.
+	 * @param threeJSGroup - The Three.js mesh of the game object.
+	 * @param rigidBodyData - The data for the rigid body associated with the game object.
+	 */
 	constructor(
 		scene: Scene,
 		threeJSGroup: THREE.Mesh,
@@ -20,9 +31,13 @@ class GameObject {
 		this.threeJSGroup = threeJSGroup;
 		this.rigidBodyData = rigidBodyData;
 		this.rapierRigidBody = null;
-		this.scene.addGameObject(this);
+		this.scene.addGameObject(this); // why do this??
 	}
 
+	/**
+	 * Synchronizes the objects three js mesh position and rotation with the rapier
+	 * collision body.
+	 */
 	syncWithRigidBody() {
 		if (this.rapierRigidBody) {
 			this.threeJSGroup.position.copy(this.rapierRigidBody.translation());
@@ -30,6 +45,10 @@ class GameObject {
 		}
 	}
 
+	/**
+	 * Returns the scene the game object belongs to.
+	 * @returns The scene.
+	 */
 	getScene() {
 		return this.scene;
 	}
